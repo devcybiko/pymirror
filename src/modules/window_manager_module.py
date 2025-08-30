31b46ffa240e777559c4eda93807b64ac51861e9
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 10:17:01 -0400

    update

--- src/modules/window_manager_module.py
+++ src/modules/window_manager_module.py
@@ -15,5 +15,5 @@ class WindowManagerModule(PMModule):
 
 	def onRawKeyboardEvent(self, event):
 		print("window_manager", event)
-		if event.key_name == "KEY_TAB" and event.pressed and not event.repes:
+		if event.key_name == "KEY_TAB" and event.pressed and not event.repeat:
 			self.pm.next_focus_module()
