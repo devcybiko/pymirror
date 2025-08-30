776b67cd3319eb2d2393203dd70b996d177d6752
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 14:18:17 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -1,1 +1,14 @@
-src/pymirror/ir_device.py
+import lirc
+
+# Connect to the running lircd daemon
+sock = lirc.init("myprogram", blocking=False)
+
+try:
+    while True:
+        codes = lirc.nextcode()
+        if codes:
+            print("IR codes:", codes)
+except KeyboardInterrupt:
+    pass
+finally:
+    lirc.deinit()
