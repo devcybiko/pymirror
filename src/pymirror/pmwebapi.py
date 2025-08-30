9755096b4d23f0de7eb5715ca594c5a2051f712a
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 11:34:22 -0400

    update

--- src/pymirror/pmwebapi.py
+++ src/pymirror/pmwebapi.py
@@ -65,6 +65,7 @@ class PMWebApi:
         cached_text = self.memory_cache.get()
         if cached_text:
             self.from_cache = False
+            self.text = cached_text
         if not cached_text:
             cached_text = self.file_cache.get()
             _print(f" | Cached file {self.file_cache.file_info.fname} is valid")
