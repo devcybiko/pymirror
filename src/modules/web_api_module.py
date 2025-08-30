4e416332d81ddc551787d5f42d463f9bfe085749
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 11:21:57 -0400

    update

--- src/modules/web_api_module.py
+++ src/modules/web_api_module.py
@@ -59,6 +59,7 @@ class WebApiModule(PMCard):
 			return
 		if self.item_number >= len(self.items):
 			self.item_number = 0
+		
 		self.header = self.items[self.item_number].get("header", "")
 		self.body = self.items[self.item_number].get("body", "")
 		self.footer = self.items[self.item_number].get("footer", "") 
