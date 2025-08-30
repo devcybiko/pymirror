5237dfb3619a74d7fc538eb7912465d1939965c5
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:48:11 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -4,7 +4,7 @@ import time
 from evdev import InputDevice, ecodes
 
 class IRDevice:
-    def __init__(self, device_path=)
+    def __init__(self, device_path="dev")
 DEVICE_PATH = '/dev/input/event0'
 dev = InputDevice(DEVICE_PATH)
 
