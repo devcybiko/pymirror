c0b1b7679ae053aa6dd0ee27f5cb50d6131ce451
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 14:44:43 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -1,10 +1,48 @@
-import socket
+#!/usr/bin/env python3
+import subprocess
+import threading
+import queue
+import sys
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-sock.connect("/var/run/lirc/lircd")
+def enqueue_output(out, q):
+    for line in iter(out.readline, b''):
+        q.put(line.decode().strip())
+    out.close()
 
-while True:
-    data = sock.recv(128)
-    print(data)
-    if data:
-        print(data.decode().strip())
+# Command to run
+cmd = [
+    "ir-keytable",
+    "-v",
+    "-t",
+    "-p", "rc-5,rc-5-sz,jvc,sony,nec,sanyo,mce_kbd,rc-6,sharp,xmp"
+]
+
+# Start the subprocess
+proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
+
+# Queue to store stdout lines
+q = queue.Queue()
+
+# Start thread to read stdout
+t = threading.Thread(target=enqueue_output, args=(proc.stdout, q))
+t.daemon = True  # Thread dies with the program
+t.start()
+
+print("Listening for IR key events...")
+
+try:
+    while True:
+        try:
+            line = q.get_nowait()  # Non-blocking
+        except queue.Empty:
+            # No line available, do other work here
+            continue
+        if line:
+            # Example parsing
+            if "scancode" in line:
+                print("IR:", line)
+except KeyboardInterrupt:
+    print("Exiting...")
+    proc.terminate()
+    t.join()
+    sys.exit(0)
