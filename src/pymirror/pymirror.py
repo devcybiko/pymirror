8a5186eaa58ac159b19f4932ff6dec9d18dc2dc5
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 14:05:55 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -301,7 +301,7 @@ class PyMirror:
                 _debug("_update_screen:", f"{(t1-t0)*1000} ms")
 
                 t0 = time.time()
-                time.sleep(0.0001) # Sleep for a short time to give pmserver a chance to process web requests
+                time.sleep(0.0) # Sleep for a short time to give pmserver a chance to process web requests
                 t1 = time.time()
                 _debug("(sleep):", f"{(t1-t0)*1000} ms")
                 _debug("")
