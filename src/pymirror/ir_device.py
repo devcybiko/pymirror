b52c68845a9f6733aee79b6bd5e68879742bf216
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 14:53:19 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
 import subprocess
 import os
-import time
+import select
 
-# Command: ir-keytable in test mode with all protocols
+# Command to run ir-keytable unbuffered
 cmd = [
+    "stdbuf", "-o0",
     "ir-keytable",
     "-v", "-t",
     "-p", "rc-5,rc-5-sz,jvc,sony,nec,sanyo,mce_kbd,rc-6,sharp,xmp"
@@ -11,22 +13,30 @@ cmd = [
 
 # Start subprocess
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-# Make stdout non-blocking
 os.set_blocking(proc.stdout.fileno(), False)
 
+print("Listening for IR events...")
+
 try:
+    buffer = ""
     while True:
-        try:
-            line = proc.stdout.readline()
-            if line:
-                print(line.strip())
-        except BlockingIOError:
-            # No data available yet
-            pass
-        
-        # Optional: do other stuff here
-        time.sleep(0.01)
+        # Use select to check if data is available
+        rlist, _, _ = select.select([proc.stdout], [], [], 0.01)
+        if proc.stdout in rlist:
+            chunk = proc.stdout.read(1024)
+            if chunk:
+                buffer += chunk
+                while "\n" in buffer:
+                    line, buffer = buffer.split("\n", 1)
+                    line = line.strip()
+                    if line:
+                        # Detect key down / repeat / key up
+                        if "repeat" in line:
+                            print(f"KEY REPEAT: {line}")
+                        elif "scancode" in line:
+                            print(f"KEY DOWN: {line}")
+                        else:
+                            print(f"OTHER: {line}")
 
 except KeyboardInterrupt:
     print("Exiting...")
