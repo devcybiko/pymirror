d1c241a2837f95b2faadba79555edce66f71e074
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:05:40 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -251,6 +251,7 @@ class PyMirror:
                 t0 = time.time()
                 # self._read_keyboard() # read the keyboard and create any keyboard events
                 t1 = time.time()
+                print("_read_server_queue:", t0-t1)
                 t0 = time.time()
                 self._read_server_queue() # read any new events from the server queue
                 t1 = time.time()
