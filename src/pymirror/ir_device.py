c1e34c65a0611d8d0a985b07362dfb6f49a6d81e
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:12:50 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -93,8 +93,9 @@ class IRDevice:
 
 if __name__ == "__main__":
     lut = {
-        69: "KEY_ONE"
+        69: "KEY_ONE",
         70: "KEY_TWO",
+        
     }
     ir = IRDevice()
     while True:
