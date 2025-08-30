6222ae7b27218e79cc90445c2fc6f924396c3291
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:10:17 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -14,7 +14,7 @@ class IRDevice:
         self.last_scancode = None
         self.last_time = 0
         self.key_down = False
-        self.lut
+        self.lut = {}
 
     def set_lut(self, d: dict):
         self.lut = d
