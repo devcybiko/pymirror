48da954573a8ba75e9d89799ae90405cdfba9dce
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:52:53 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -32,6 +32,7 @@ class IRDevice:
         r, _, _ = select.select([self.dev], [], [], 0.05)  # 50ms timeout
         now = time.time()
         result = {}
+        
         if self.dev in r:
             for event in self.dev.read():
                 if event.type == ecodes.EV_MSC:
