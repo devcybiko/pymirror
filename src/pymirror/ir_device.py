5e52b147edb5b29ba7fb49fadacfc7c0d3d16a1a
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 14:54:15 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -2,6 +2,7 @@
 import subprocess
 import os
 import select
+import time
 
 # Command to run ir-keytable unbuffered
 cmd = [
@@ -11,16 +12,20 @@ cmd = [
     "-p", "rc-5,rc-5-sz,jvc,sony,nec,sanyo,mce_kbd,rc-6,sharp,xmp"
 ]
 
-# Start subprocess
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 os.set_blocking(proc.stdout.fileno(), False)
 
 print("Listening for IR events...")
 
+# Track last press times
+key_last_time = {}
+KEY_UP_TIMEOUT = 0.2  # seconds
+
+buffer = ""
+
 try:
-    buffer = ""
     while True:
-        # Use select to check if data is available
+        # Non-blocking read
         rlist, _, _ = select.select([proc.stdout], [], [], 0.01)
         if proc.stdout in rlist:
             chunk = proc.stdout.read(1024)
@@ -29,14 +34,30 @@ try:
                 while "\n" in buffer:
                     line, buffer = buffer.split("\n", 1)
                     line = line.strip()
-                    if line:
-                        # Detect key down / repeat / key up
-                        if "repeat" in line:
-                            print(f"KEY REPEAT: {line}")
-                        elif "scancode" in line:
-                            print(f"KEY DOWN: {line}")
-                        else:
-                            print(f"OTHER: {line}")
+                    if not line:
+                        continue
+
+                    now = time.time()
+
+                    if "repeat" in line:
+                        print(f"KEY REPEAT: {line}")
+                        # Update last press time
+                        scancode = line.split("scancode = ")[-1].split()[0]
+                        key_last_time[scancode] = now
+
+                    elif "scancode" in line:
+                        print(f"KEY DOWN: {line}")
+                        scancode = line.split("scancode = ")[-1].split()[0]
+                        key_last_time[scancode] = now
+
+        # Check for keys that timed out (assume released)
+        expired = []
+        for sc, t in key_last_time.items():
+            if time.time() - t > KEY_UP_TIMEOUT:
+                print(f"KEY UP: scancode = {sc}")
+                expired.append(sc)
+        for sc in expired:
+            del key_last_time[sc]
 
 except KeyboardInterrupt:
     print("Exiting...")
