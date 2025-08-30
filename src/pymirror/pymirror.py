629f23e2ee71e6b909982897e930d9849a562cde
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 10:50:46 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -124,6 +124,7 @@ class PyMirror:
                 self.publish_event(event)
         except queue.Empty:
             # No new events in the queue
+            print("... empty server_queue")
             pass
 
     def _read_keyboard(self):
