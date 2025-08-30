c3c92bb1f78a1c5f81d8550495d8e7b8a1cf5238
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:51:11 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -30,38 +30,38 @@ class IRDevice:
 
     def get_key_event(self):
         try:
-                r, _, _ = select.select([dev], [], [], 0.05)  # 50ms timeout
-                now = time.time()
+            r, _, _ = select.select([dev], [], [], 0.05)  # 50ms timeout
+            now = time.time()
 
-                if dev in r:
-                    for event in dev.read():
-                        if event.type == ecodes.EV_MSC:
-                            scancode = event.value
-                            protocol = guess_protocol(scancode)
+            if dev in r:
+                for event in dev.read():
+                    if event.type == ecodes.EV_MSC:
+                        scancode = event.value
+                        protocol = guess_protocol(scancode)
 
-                            if scancode == self.last_scancode:
-                                if self.key_down and (now - self.last_time) < self.REPEAT_THRESHOLD:
-                                    print(f"{protocol}: scancode=0x{scancode:X} repeat")
-                                else:
-                                    print(f"{protocol}: scancode=0x{scancode:X} pressed")
-                                    self.key_down = True
+                        if scancode == self.last_scancode:
+                            if self.key_down and (now - self.last_time) < self.REPEAT_THRESHOLD:
+                                print(f"{protocol}: scancode=0x{scancode:X} repeat")
                             else:
                                 print(f"{protocol}: scancode=0x{scancode:X} pressed")
                                 self.key_down = True
+                        else:
+                            print(f"{protocol}: scancode=0x{scancode:X} pressed")
+                            self.key_down = True
 
-                            self.last_scancode = scancode
-                            self.last_time = now
+                        self.last_scancode = scancode
+                        self.last_time = now
 
-                        elif event.type == ecodes.EV_SYN:
-                            pass  # end of event batch
-                # print("...")
+                    elif event.type == ecodes.EV_SYN:
+                        pass  # end of event batch
+            # print("...")
 
-                # Detect key up
-                if self.key_down and self.last_scancode is not None and (now - self.last_time) > self.KEYUP_THRESHOLD:
-                    protocol = guess_protocol(self.last_scancode)
-                    print(f"{protocol}: scancode=0x{self.last_scancode:X} released")
-                    self.key_down = False
-                    self.last_scancode = None
+            # Detect key up
+            if self.key_down and self.last_scancode is not None and (now - self.last_time) > self.KEYUP_THRESHOLD:
+                protocol = guess_protocol(self.last_scancode)
+                print(f"{protocol}: scancode=0x{self.last_scancode:X} released")
+                self.key_down = False
+                self.last_scancode = None
 
         except KeyboardInterrupt:
             print("\nExiting...")
