64a830a8753a62da68057336f6689b0e84784413
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 09:44:34 -0400

    update

--- src/modules/window_manager_module.py
+++ src/modules/window_manager_module.py
@@ -5,11 +5,6 @@ class WindowManagerModule(PMModule):
 	def __init__(self, pm, config):
 		super().__init__(pm, config)
 		self._window_manager = config.window_manager
-		self.name = self._window_manager.name
-		self.alerts = config.alerts or []
-		if type(self.alerts) is not list:
-			self.alerts = [self.alerts]
-		self.window_managertab = WindowManagertab([alert.window_manager for alert in self.alerts])
 
 	def render(self, force: bool = False) -> bool:
 		pass
