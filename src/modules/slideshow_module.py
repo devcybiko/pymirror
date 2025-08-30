60a2da36da8db04d2f07f6de5f1478ab4b434d15
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 11:05:45 -0400

    update

--- src/modules/slideshow_module.py
+++ src/modules/slideshow_module.py
@@ -59,4 +59,6 @@ class SlideshowModule(PMModule):
 		print("slideshow_module", event)
 		if event.key_name == "KEY_LEFT":
 			self.photo_number = (self.photo_number + len(self.photos) -1) % len(self.photos)
+		if event.key_name == "KEY_LEFT":
+			self.photo_number = (self.photo_number + len(self.photos) -1) % len(self.photos)
 		pass
