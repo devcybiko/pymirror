ed0b61361169944e51d4b222e50d2ee8db668881
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 09:45:52 -0400

    update

--- src/modules/window_manager_module.py
+++ src/modules/window_manager_module.py
@@ -10,12 +10,6 @@ class WindowManagerModule(PMModule):
 		pass
 
 	def exec(self):
-		alert_indexes = self.window_managertab.check()
-		if not alert_indexes:
-			return
-		for i in alert_indexes:
-			alert = self.alerts[i]
-			_debug(f"Alert triggered: {alert.event}")
-			self.publish_event(alert.event)
-		return False
+		pass
 
+	
