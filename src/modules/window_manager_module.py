ef886912509d287307268f343a332e0f12db00b2
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 09:49:26 -0400

    update

--- src/modules/window_manager_module.py
+++ src/modules/window_manager_module.py
@@ -15,6 +15,7 @@ class WindowManagerModule(PMModule):
 	def _find_focus_module_n(self):
 		mod_n = None
 			for i in range(0, len(self.pm.modules)):
+				if 
 	def onRawKeyboardEvent(self, event):
 		mod_n = 
 		if event.key_name == "TAB":
