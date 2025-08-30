7b75a8b063c419fdb25a88325e10bfaeb6a6239c
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:52:02 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -57,7 +57,7 @@ class IRDevice:
 
             # Detect key up
             if self.key_down and self.last_scancode is not None and (now - self.last_time) > self.KEYUP_THRESHOLD:
-                protocol = guess_protocol(self.last_scancode)
+                protocol = self.guess_protocol(self.last_scancode)
                 print(f"{protocol}: scancode=0x{self.last_scancode:X} released")
                 self.key_down = False
                 self.last_scancode = None
