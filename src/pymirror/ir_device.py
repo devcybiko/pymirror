f077e933c87cfc80c7cd60898777d4812cb165be
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:09:59 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -14,7 +14,7 @@ class IRDevice:
         self.last_scancode = None
         self.last_time = 0
         self.key_down = False
-        self.trancl
+        self.translation_
 
     def set_translation_table(self, d: dict):
         pass
