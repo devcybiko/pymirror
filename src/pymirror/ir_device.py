3e94ddff3cb3ec806c7ea7d07ba88de909081fac
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:23:39 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
         28: "KEY_ENTER"
     }
     ir = IRDevice()
-    ir.
+    ir.set_key_name_lut(lut)
     while True:
         event = ir.get_key_event()
         if event:
