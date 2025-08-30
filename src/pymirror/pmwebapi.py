7136696b13d06d34954ed2cd453706d964fdc184
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 11:35:54 -0400

    update

--- src/pymirror/pmwebapi.py
+++ src/pymirror/pmwebapi.py
@@ -61,6 +61,7 @@ class PMWebApi:
             self.cancel()
             self.error = e
 
+    def 
     def fetch_text(self, blocking=True):
         cached_text = self.memory_cache.get()
         if cached_text:
