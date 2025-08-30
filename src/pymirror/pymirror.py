ed0d58962da75f1363222b5b6747638a823775d6
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:43:29 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -143,6 +143,19 @@ class PyMirror:
             }
             self.publish_event(event)
 
+    def _read_keyboard(self):
+        ## add any messages that have come from the keyboard
+        while key_event := self.keyboard.get_key_event():
+            _debug(f"Received event from keyboard: {key_event}")
+            event = {
+                "event": "RawKeyboardEvent",
+                "keycode":  key_event["keycode"],
+                'key_name': key_event["key_name"],
+                'scancode': key_event["scancode"],
+                'pressed': key_event["pressed"],
+                'repeat': key_event["repeat"]
+            }
+            self.publish_event(event)
     def _send_events_to_module(self, module, events):
         if not module.subscriptions: 
             return
