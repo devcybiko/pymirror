5bb9927fe0f06094b0c05d6cc7e3ffb1c89f5e27
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:20:58 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -105,7 +105,8 @@ if __name__ == "__main__":
         22: "KEY_NUMBERSIGN",
         13: "KEY_ASTERISK",
         24: "KEY_UP",
-        8: "KEY"
+        8: "KEY_LEFT",
+        
 
     }
     ir = IRDevice()
