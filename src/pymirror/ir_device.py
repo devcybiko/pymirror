e2f3fe0233e4152f5621699218ff70367cc7cef2
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:55:10 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -48,6 +48,7 @@ class IRDevice:
                             result["repeat"] = True
                         else:
                             print(f"{protocol}: scancode=0x{scancode:X} pressed")
+                            result["pressed"]
                             self.key_down = True
                     else:
                         print(f"{protocol}: scancode=0x{scancode:X} pressed")
