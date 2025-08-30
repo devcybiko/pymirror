eb088f78bd1ae917e89436edfce0b1c05ef48b64
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 11:36:23 -0400

    update

--- src/pymirror/pmwebapi.py
+++ src/pymirror/pmwebapi.py
@@ -64,7 +64,7 @@ class PMWebApi:
     def _get_memory_cache(self):
         return self.memory_cache.get()
     
-    def
+    def _get_
     def fetch_text(self, blocking=True):
         cached_text = 
         if cached_text:
