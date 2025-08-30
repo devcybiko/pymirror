9d4078ed9fd1049d763f7cd7012c3c182869faf3
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 12:06:14 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -257,11 +257,12 @@ class PyMirror:
                 self._read_server_queue() # read any new events from the server queue
                 t1 = time.time()
                 print("_read_server_queue:", t0-t1)
-                
+
                 t0 = time.time()
                 self._send_events_to_modules()  # send all new events to the modules
                 t1 = time.time()
                 print("_read_server_queue:", t0-t1)
+                
                 modules_changed = self._exec_modules() # update / check the state of all modules
                 self._render_modules(modules_changed)  # Render only the modules that changed state
                 self._update_screen()  # Update the screen with the rendered modules
