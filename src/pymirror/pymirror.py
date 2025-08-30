e246d968d366d2ab37e6795c8ade6944d0ca10ac
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 13:43:38 -0400

    update

--- src/pymirror/pymirror.py
+++ src/pymirror/pymirror.py
@@ -144,7 +144,7 @@ class PyMirror:
             self.publish_event(event)
 
     def _read_remote(self):
-        ## add any messages that have come from the keyboard
+        ## add any messages that have come from the ir remote
         while key_event := self.keyboard.get_key_event():
             _debug(f"Received event from keyboard: {key_event}")
             event = {
