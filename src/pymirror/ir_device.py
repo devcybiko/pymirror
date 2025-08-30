38afb5670f03f6c3bcc07aae4bdaf9e8e3394714
Gregory Smith <greg@agilefrontiers.com>
2025-08-30 15:12:20 -0400

    update

--- src/pymirror/ir_device.py
+++ src/pymirror/ir_device.py
@@ -195,7 +195,6 @@ class IRDevice:
                 'repeat': False
             }
                                                     
-
         return None
     
     def get_key_char(self) -> Optional[str]:
