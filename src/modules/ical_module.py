559c405b3891968fe7c0a01e954ec14787010f0e
Gregory Smith <greg@agilefrontiers.com>
2025-09-09 20:46:50 -0400

    rpi03

--- src/modules/ical_module.py
+++ src/modules/ical_module.py
@@ -10,6 +10,7 @@ from pymirror.pmcard import PMCard
 from pymirror.pmwebapi import PMWebApi
 from pymirror.utils import SafeNamespace, strftime_by_example
 from pymirror.pmlogger import _debug, _error
+from pymirror.ical_parser import IcalParser
 
 @dataclass
 class ICalConfig:
@@ -32,88 +33,6 @@ class IcalModule(PMCard):
         self.all_day_format = strftime_by_example(self._ical.all_day_format)
         self.time_format = strftime_by_example(self._ical.time_format)
 
-    def _dump_event(self, event):
-        _debug(event.name)
-        _debug("...begin:", event.begin)
-        _debug("...end:", event.end)
-        _debug("...duration:", event.duration)
-        _debug("...uid:", event.uid)
-        _debug("...description:", event.description)
-        _debug("...created:", event.created)
-        _debug("...last_modified:", event.last_modified)
-        _debug("...location:", event.location)
-        _debug("...url:", event.url)
-        _debug("...transparent:", event.transparent)
-        _debug("...alarms:", event.alarms)
-        _debug("...attendees:", event.attendees)
-        _debug("...categories:", event.categories)
-        _debug("...status:", event.status)
-        _debug("...organizer:", event.organizer)
-        _debug("...geo:", event.geo)
-        _debug("...classification:", event.classification)
-        _debug("...extra:", event.extra)
-
-    def _parse_rrule(self, rrule: str, now: datetime) -> dict:
-        """Parse the RRULE string into a dictionary."""
-        ## NOTE: Original (raw) values are in UPPERCASE
-        ## NOTE: The parsed values are in lowercase
-        dow = {
-            "SU": 6,
-            "MO": 0,
-            "TU": 1,
-            "WE": 2,
-            "TH": 3,
-            "FR": 4,
-            "SA": 5
-        }
-        rules = {}
-        for part in rrule.split(";"):
-            key, value = part.split("=", 1)
-            key = key.strip().upper()
-            value = value.strip().upper()
-            rules[key] = value
-            if key == "UNTIL":
-                if len(value) == 16:  # YYYYMMDDTHHMMSSZ
-                    rules[key.lower()] = datetime.strptime(value, "%Y%m%dT%H%M%SZ").replace(tzinfo=timezone.utc)
-                elif len(value) == 8:  # YYYYMMDD
-                    rules[key.lower()] = datetime.strptime(value, "%Y%m%d").replace(tzinfo=timezone.utc)
-            elif key in ["BYSECOND", "BYMINUTE", "BYHOUR", "BYMONTHDAY", "BYYEARDAY", "BYWEEKNO", "BYMONTH", "BYSETPOS"]:
-                rules[key.lower()] = [int(s) for s in value.split(",")]
-            elif key in ["BYDAY"]:
-                rules[key.lower()] = [dow.get(s, s) for s in value.split(",")]
-            elif key in ["WKST"]:
-                rules[key.lower()] = dow.get(value, value)
-            else:
-                if value.isdigit():
-                    rules[key.lower()] = int(value)
-        if not rules.get("UNTIL"):
-            ## default to now
-            rules["UNTIL"] = now.strftime("%Y%m%dT%H%M%SZ")
-            rules["until"] = now
-        return SafeNamespace(**rules)
-
-    def _get_rrule(self, event, now: datetime) -> bool:
-        for _line in event.extra:
-            line = str(_line).strip()
-            if "RRULE:" == line[0:6]:
-                return self._parse_rrule(line[6:], now)
-        return None
-
-    def _generate_recurring_dates(self, event, now: datetime) -> list:
-        rrule = self._get_rrule(event, now)
-        if not rrule:
-            return []
-
-        # Parse the RRULE and generate the recurring dates
-        # This is a simplified example and may need to be adjusted for real RRULE parsing
-        dates = []
-        for line in rrule.splitlines():
-            if line.startswith("FREQ=WEEKLY"):
-                # Generate weekly dates
-                for i in range(1, 5):  # Next 4 weeks
-                    dates.append(event.begin + timedelta(weeks=i))
-        return dates
-
     def exec(self) -> bool:
         is_dirty = super().exec()
         if not self.timer.is_timedout(): return is_dirty # early exit if not timed out
@@ -132,43 +51,38 @@ class IcalModule(PMCard):
             self.timer.reset(1000)
             return False
         print("got data...")
-        epoch = datetime(1980, 1, 1, tzinfo=timezone.utc)
+        # epoch = datetime(1980, 1, 1, tzinfo=timezone.utc)
         now = datetime.now(timezone.utc)
-        later = now + timedelta(hours=24 * self._ical.number_days)
-        cal = Calendar(self.ical_response)
-        events = cal.timeline.included(epoch, later)
-
+        later = (now + timedelta(hours=24 * self._ical.number_days))
+        now_str = now.strftime("%Y-%m-%d")
+        later_str = later.strftime("%Y-%m-%d")
+        ical_parser = IcalParser(self.ical_response.splitlines())
+        events = ical_parser.parse(now_str, later_str)
         event_str = ""
         all_day_str = ""
         event_cnt = 0
         daily_events = []
         all_day_events = []
         for event in events:
-            rrule = self._get_rrule(event, now)
-            if rrule and rrule.until >= now:
-                # print(f"Event: {event.name}({event.all_day}) - {event.begin} to {event.end}")
-                # print(f"RRULE: {rrule}\n")
-                # new_events = self._generate_recurring_dates(event, now, later)
-                pass
             if event_cnt > self._ical.max_events:
                 break
-            if event.begin.datetime < now:
+            if event.get("dtstart$", "") < now_str:
                 continue
-            if event.all_day and self._ical.show_all_day_events:
+            if event.get("all_day") and self._ical.show_all_day_events:
                 all_day_events.append(event)
             else:
                 daily_events.append(event)
 
         for event in daily_events:
-            event_str += f"{event.begin.strftime(self.time_format)}: {event.name}\n"
+            event_str += f"{event.get('dtstart').strftime(self.time_format)}: {event.get('name', event.get('summary', 'none'))}\n"
         for event in all_day_events:
-            all_day_str += f"{event.begin.strftime(self.all_day_format)}: {event.name}\n"
+            all_day_str += f"{event.get('dtstart').strftime(self.all_day_format)}: {event.get('name', event.get('summary', 'none'))}\n"
         if self._ical.number_days > 1:
             format = strftime_by_example("Monday, Jan 1")
             header_str = f"iCalendar\n{now.strftime(format)} - {later.strftime(format)}"
         self.update(
             header_str,
-            event_str or "No Events to Show",
+            (event_str + "\n" + all_day_str) or "No Events to Show",
             "last updated\n" + now.astimezone().strftime("%Y-%m-%d %H:%M:%S"),
         )
         print("updated...")
---
+++ src/pymirror/ical_parser.py
@@ -0,0 +1,93 @@
+from datetime import datetime, timezone
+import json
+from sys import stderr
+
+class IcalParser:
+    def __init__(self, lines):
+        self.lines = lines
+        pass
+
+    def parse(self, start_date, end_date):
+        self.results = []
+        self._parse_lines(self.lines, 0, start_date, end_date)
+        self.results = sorted(self.results, key=lambda event: event["dtstart$"])
+        return self.results
+
+    def _parse_line(self, lines, i) -> tuple[int, str]:
+        line = lines[i]
+        i += 1
+        while i < len(lines) and lines[i] and lines[i][0] == ' ':
+            line += lines[i][1:]
+            i += 1
+        return i, line
+
+    def _parse_datetime(self, dtstr):
+        zulu = False
+        if "Z" in dtstr:
+            dtstr = dtstr[0:-1]
+            zulu = True
+        if "T" in dtstr:
+            if zulu:
+                dt = datetime.strptime(dtstr, "%Y%m%dT%H%M%S").replace(tzinfo=timezone.utc)
+            else:
+                dt = datetime.strptime(dtstr, "%Y%m%dT%H%M%S")
+        else:
+            if zulu:
+                dt = datetime.strptime(dtstr, "%Y%m%d").replace(tzinfo=timezone.utc)
+            else:
+                dt = datetime.strptime(dtstr, "%Y%m%d")
+        return dt, dt.isoformat()
+
+    def _parse_keyword(self, event, keyword0, keyword1):
+        return event.get(keyword0, event.get(keyword1, ":")).split(":", 1)[1]
+
+    def _parse_event(self, event):
+        dtend = self._parse_keyword(event, "DTEND:", "DTEND;")
+        dtstart = self._parse_keyword(event, "DTSTART:", "DTSTART;")
+        event["all_day"] = not ("T" in dtend)
+        event["dtend"], event["dtend$"] = self._parse_datetime(dtend)
+        event["dtstart"], event["dtstart$"]= self._parse_datetime(dtstart)
+        event["summary"] = self._parse_keyword(event, "SUMMARY:", "SUMMARY;")
+        event["description"] = self._parse_keyword(event, "DESCRIPTION:", "DESCRIPTION;")
+        event["rrule"] = self._parse_keyword(event, "RRULE:", "RRULE;")
+        return event
+
+    def _parse_lines(self, lines, i, start_date, end_date) -> tuple[int, dict]:
+        event = {}
+        while i < len(lines):
+            i, line = self._parse_line(lines, i)
+            if not line: 
+                i += 1
+                continue
+            if line == "BEGIN:VEVENT":
+                event = {}
+            elif line == "END:VEVENT":
+                event = self._parse_event(event)
+                # print(start_date, event.get("dtstart", ""), event.get("dtend", ""), end_date, file=stderr)
+                if start_date <= event.get("dtstart$", "") and event.get("dtend$", "") <= end_date:
+                        # print(event)
+                        self.results.append(event)
+                event = {}
+            else:
+                tokens = ["RRULE:","DTSTART;","DTEND;","DTSTART:","DTEND:","SUMMARY;","SUMMARY:","DESCRIPTION:","DTSTAMP:"]
+                for token in tokens:
+                    if line.startswith(token):
+                        event[token] = line
+        return 0, 0
+
+
+
+def main():
+    def json_default(obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return str(obj)
+
+    with open("./caches/ical2.json", 'r', encoding='utf-8') as file:
+        text = file.read()
+    ical_parser = IcalParser(text.split("\n"))
+    result = ical_parser.parse("2025-08-01", "2025-12-31")
+    print(json.dumps(result, indent=2, default=json_default), file=stderr)
+
+if __name__ == "__main__":
+    main()
