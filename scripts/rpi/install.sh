39a4922f54aaf4d21eb2e36fcc373d37e7157943
Gregory Smith <greg@agilefrontiers.com>
2025-09-14 16:27:40 -0400

    update

--- scripts/rpi/install.sh
+++ scripts/rpi/install.sh
@@ -7,5 +7,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r ./scripts/rpi/requirements.txt
+sudo apt update
+sudo apt upgrade -y
 sudo xargs -a ./scripts/rpi/packages.txt apt-get install -y
 
