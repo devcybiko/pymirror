5c9a7f24358db7d8db6e712b89542fd0ffa9ed60
Gregory Smith <greg@agilefrontiers.com>
2025-09-20 00:35:46 -0400

    udpating configs for rpi03

--- taskmgr.sh
+++ taskmgr.sh
@@ -9,8 +9,7 @@ fi
 while true; do
     PYTHONPATH=src \
         python3 -u -m pmtaskmgr.pmtaskmgr \
-        --config "$CONFIG" \
-        2>&1 >> ./src/pmserver/static/output.log
+        --config "$CONFIG"
         date
         echo "Restarting pmtaskmgr..."
         sleep 10
