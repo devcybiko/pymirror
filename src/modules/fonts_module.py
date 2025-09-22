81c6114a302aeb8d7b5fa4900db4bb00c87ff1cf
Gregory Smith <greg@agilefrontiers.com>
2025-09-22 18:21:43 -0400

    updating rpi01

--- src/modules/fonts_module.py
+++ src/modules/fonts_module.py
@@ -18,31 +18,35 @@ class FontsModule(PMModule):
 		# Get the path to fontlist.txt relative to the project root
 		with open("./fontlist.txt", 'r') as f:
 			for line in f:
-				line = line.strip().split(":")[0].strip()  # Remove comments and whitespace
-				font_name = os.path.basename(line).split('.')[0]  # Get the font name without extension
-				loaded = self.gfx.set_font(font_name, self.gfx.font_size)  # Load the font
-				if loaded:
+				print(line)
+				font_file = line.strip().split(":")[0].strip()  # Remove comments and whitespace
+				font_name = os.path.basename(font_file).split('.')[0]  # Get the font name without extension
+				# test if font_file exists
+				if os.path.isfile(font_file):
 					font_list.append(font_name)
+				else:
+					print(f"Font file {font_file} not found.")
 		return sorted(font_list)
 
 	def render(self, force: bool = False) -> int:
-		gfx = self.gfx
+		gfx = self.bitmap.gfx_push()
 		text = self.bitmap.text
 		self.bitmap.clear()
 		gfx.text_color = "#fff"  # Set text color to white
 		for i in range(self.max_items):
 			n = (self.font_item + i) % len(self.font_list)
 			gfx.set_font(self.font_list[n], gfx.font_size)
-			text(gfx, self.font_list[n], 0, 0 + i * gfx.font_size)
+			text(self.font_list[n], 0, 0 + i * gfx.font_size)
 			# color = color_to_tuple(gfx.text_color)
 			# color = _mul(color, (0.90, 0.90, 0.90))
 			# gfx.text_color = color_from_tuple(color)
+		gfx = self.bitmap.gfx_pop()
 		return True
 
 	def exec(self):
 		if self.timer.is_timedout():
 			self.timer.reset()
-			self.font_item += 1
+			self.font_item += 3
 			return True
 		return False
 
