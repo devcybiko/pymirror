b8a6d0bbe4aaf0e5115e6b5f66808e5b3cb21197
Gregory Smith <greg@agilefrontiers.com>
2026-03-12 00:42:10 -0400

    starting depreciation estimates

--- turo_depreciation.py
+++ turo_depreciation.py
@@ -59,7 +59,7 @@ def main():
     ## gather data
     db_path = expand_string("$HOME/turo.db", {})
     db = GLSDb(f"sqlite:///{db_path}")
-    trips = db.query("SELECT * FROM trips")
+    trips = db.query("SELECT * FROM trips where trip_status = 'Completed'")
     vehicles = db.query("SELECT * FROM vehicles")
 
     ## coallate trips
@@ -92,16 +92,21 @@ def main():
                 continue
             if last_trip is None:
                 trip.vehicle_value = cars[vehicle_nickname].last_kbb_value
-                trip.vehicle_miles = trip.check_out_odometer
+                trip.vehicle_miles = trip.check_in_odometer
                 trip.miles_remaining = MAX_MILES - trip.vehicle_miles
+                trip.cumulative_earnings = 0
                 last_trip = trip
-            trip.vehicle_miles = trip.check_in_odometer
+            trip.vehicle_miles = trip.check_out_odometer
             delta_miles = trip.vehicle_miles - last_trip.vehicle_miles
             trip.vehicle_value = last_trip.vehicle_value - (delta_miles * DEPRECIATION_PER_MILE)
             trip.miles_remaining = MAX_MILES - trip.vehicle_miles
+            trip.cumulative_earnings = last_trip.cumulative_earnings + trip.total_earnings
             last_trip = trip
     
     ## print depreciation
+    totol_equity = 0
+    total_income = 0
+    total_depreiation = 0
     for vehicle_nickname, trips in trip_data.items():
         print(f"Vehicle: {vehicle_nickname}")
         first_trip = None
@@ -113,17 +118,30 @@ def main():
             if first_trip is None:
                 first_trip = trip
             last_trip = trip
-            print(f"  Trip on {trip_date}: Value: ${trip.vehicle_value:.0f}, Miles: {trip.vehicle_miles}")
+            print(f"  Trip on {trip.trip_start}-{trip.trip_end}: Value: ${trip.vehicle_value:.0f}, Miles: {trip.vehicle_miles}")
         print("total depreciation:")
+        print(first_trip.trip_start, last_trip.trip_start)
         delta_miles = last_trip.vehicle_miles - first_trip.vehicle_miles
+        delta_days = (last_trip.trip_end - first_trip.trip_start).days + 1
         print("... ${:.0f} initial value".format(cars[vehicle_nickname].last_kbb_value))
         print("... ${:.0f} amount owed".format(cars[vehicle_nickname].remaining_balance))
         print("... ${:.0f} final value".format(last_trip.vehicle_value))
         print("... ${:.0f} depreciation".format(first_trip.vehicle_value - last_trip.vehicle_value))
         print("... ${:.0f} equity".format(last_trip.vehicle_value - cars[vehicle_nickname].remaining_balance))
         print("... {:.0f} last odo".format(last_trip.vehicle_miles))
-        print("... {:.0f} miles".format(delta_miles))
+        print("... {:.0f} miles driven".format(delta_miles))
+        print("... {:.0f} days driven".format(delta_days))
+        print("... {:.0f} miles per day".format(delta_miles / delta_days if delta_days else 0))
         print("... ${:.3f} depreciation per mile".format((first_trip.vehicle_value - last_trip.vehicle_value) / delta_miles))
+        print("... ${:.0f} cumulative earnings".format(last_trip.cumulative_earnings))
+        print("... ${:.3f} $/mile".format(last_trip.cumulative_earnings / delta_miles if delta_miles else 0))
         print("... {:.0f} miles remaining".format(last_trip.miles_remaining))
+        totol_equity += last_trip.vehicle_value - cars[vehicle_nickname].remaining_balance
+        total_income += last_trip.cumulative_earnings
+        total_depreiation += first_trip.vehicle_value - last_trip.vehicle_value
+    print("Total equity: ${:.0f}".format(totol_equity))
+    print("Total income: ${:.0f}".format(total_income))
+    print("Total depreciation: ${:.0f}".format(total_depreiation))
+    print("Total equity + income: ${:.0f}".format(totol_equity + total_income))
 
 main()
