99b2b7889876c6aa1c7038893157b38fe9c5f78d
drfrancintosh <gregory@greg-smith.com>
2026-04-22 14:11:46 -0400

    update

--- turo.sh
+++ turo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e 
 
+if [ $# -eq 0 ]; then
+    CONFIG="./configs/rpi02/config.json"
+else
+    CONFIG="$1"
+fi
+
+
 rm -f ~/turo.db
 sqlite3 ~/turo.db < ~/turo.sql
-./run.sh ./configs/rpi02/config.json
+./run.sh "$CONFIG"
